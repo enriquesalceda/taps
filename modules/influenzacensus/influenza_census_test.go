00ab409442ba1a6664edd4189a0079ad5b819981
enrique salceda <enriquesalceda@gmail.com>
2023-11-04 00:21:01 +1100

    feat: save census

---
+++ modules/influenzacensus/influenza_census_test.go
@@ -0,0 +1,42 @@
+package influenzacensus_test
+
+import (
+	"github.com/stretchr/testify/require"
+	"taps/modules/influenzacensus"
+	"testing"
+)
+
+func TestInfluenzaCesus(t *testing.T) {
+	t.Run("save census", func(t *testing.T) {
+		fieldCensus := influenzacensus.NewFieldCensus(
+			"RAHE190116MMCMRSA7",
+			"RAMIREZ",
+			"HERRERA",
+			"ESTHER ELIZABETH",
+			"16/01/2019",
+			"MEXICO",
+			"MUJER",
+			15,
+		)
+		influenzaMemoryStore := influenzacensus.NewInMemoryInfluenzaStore()
+
+		influenzaCensus := influenzacensus.NewInfluenzaCensusTaker(influenzaMemoryStore)
+		require.NoError(t, influenzaCensus.Take(fieldCensus))
+
+		require.Equal(
+			t,
+			[]influenzacensus.InfluenzaCensus{
+				{
+					ID:            "RAHE190116MMCMRSA7",
+					LastLastName:  "HERRERA",
+					FirstLastName: "RAMIREZ",
+					FirstName:     "ESTHER ELIZABETH",
+					Gender:        "MUJER",
+					DOB:           "16/01/2019",
+					State:         "MEXICO",
+					Number:        15,
+				},
+			},
+			influenzaMemoryStore.All())
+	})
+}
---
+++ modules/influenzacensus/influenze_census.go
@@ -0,0 +1,97 @@
+package influenzacensus
+
+type FieldCensus struct {
+	ID            string
+	FirstLastName string
+	LastLastName  string
+	FirstName     string
+	DOB           string
+	State         string
+	Gender        string
+	Number        int
+}
+
+func NewFieldCensus(
+	ID string,
+	firstLastName string,
+	lastLastName string,
+	firstName string,
+	DOB string,
+	state string,
+	gender string,
+	number int,
+) *FieldCensus {
+	return &FieldCensus{
+		ID:            ID,
+		FirstLastName: firstLastName,
+		LastLastName:  lastLastName,
+		FirstName:     firstName,
+		DOB:           DOB,
+		State:         state,
+		Gender:        gender,
+		Number:        number,
+	}
+}
+
+type InfluenzaCensusTaker struct {
+	store CensusStore
+}
+
+func NewInfluenzaCensusTaker(store CensusStore) *InfluenzaCensusTaker {
+	return &InfluenzaCensusTaker{store: store}
+}
+
+func (t *InfluenzaCensusTaker) Take(fieldCensus *FieldCensus) error {
+	return t.store.Save(fieldCensus)
+}
+
+// -- store
+type InMemoryInfluenzaStore struct {
+	all map[string]InfluenzaCensus
+}
+
+type InfluenzaCensus struct {
+	ID            string
+	FirstLastName string
+	LastLastName  string
+	FirstName     string
+	DOB           string
+	State         string
+	Gender        string
+	Number        int
+}
+
+type CensusStore interface {
+	All() []InfluenzaCensus
+	Save(fieldCensus *FieldCensus) error
+}
+
+func NewInMemoryInfluenzaStore() *InMemoryInfluenzaStore {
+	return &InMemoryInfluenzaStore{
+		all: map[string]InfluenzaCensus{},
+	}
+}
+
+func (i *InMemoryInfluenzaStore) All() []InfluenzaCensus {
+	var allCensus []InfluenzaCensus
+	for _, census := range i.all {
+		allCensus = append(allCensus, census)
+	}
+	return allCensus
+}
+
+func (i *InMemoryInfluenzaStore) Save(fieldCensus *FieldCensus) error {
+	influenzaCensus := InfluenzaCensus{
+		ID:            fieldCensus.ID,
+		FirstLastName: fieldCensus.FirstLastName,
+		LastLastName:  fieldCensus.LastLastName,
+		FirstName:     fieldCensus.FirstName,
+		DOB:           fieldCensus.DOB,
+		State:         fieldCensus.State,
+		Gender:        fieldCensus.Gender,
+		Number:        fieldCensus.Number,
+	}
+
+	i.all[fieldCensus.ID] = influenzaCensus
+	return nil
+}
