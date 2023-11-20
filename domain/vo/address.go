5838b0a0655328d7146655bb03e1a73c78abb7bf
enrique salceda <enriquesalceda@gmail.com>
2023-11-20 22:53:04 +1100

    feat: add address feature

--- domain/vo/address.go
+++ domain/vo/address.go
@@ -1,1 +1,38 @@
 package vo
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Address struct {
+	StreetNumber string
+	StreetName   string
+	SuburbName   string
+}
+
+func TryParseAddress(streetNumber, streetName, suburbName string) (Address, error) {
+	errors := []string{}
+
+	if streetNumber == "" {
+		errors = append(errors, "street number")
+	}
+
+	if streetName == "" {
+		errors = append(errors, "street name")
+	}
+
+	if suburbName == "" {
+		errors = append(errors, "suburb name")
+	}
+
+	if len(errors) > 0 {
+		return Address{}, fmt.Errorf("address should have: %s", strings.Join(errors, ", "))
+	}
+
+	return Address{
+		StreetNumber: streetNumber,
+		StreetName:   streetName,
+		SuburbName:   suburbName,
+	}, nil
+}
--- domain/vo/address_test.go
+++ domain/vo/address_test.go
@@ -1,1 +1,67 @@
-package vo
+package vo_test
+
+import (
+	"github.com/stretchr/testify/require"
+	"taps/domain/vo"
+	"testing"
+)
+
+func TestAddress(t *testing.T) {
+	t.Run("try parse address", func(t *testing.T) {
+		address, err := vo.TryParseAddress(
+			"1",
+			"Calle Benito Juarez",
+			"El Centro",
+		)
+		require.NoError(t, err)
+
+		require.Equal(t,
+			vo.Address{
+				StreetNumber: "1",
+				StreetName:   "Calle Benito Juarez",
+				SuburbName:   "El Centro",
+			},
+			address,
+		)
+	})
+
+	t.Run("try parse address with empty street number", func(t *testing.T) {
+		_, err := vo.TryParseAddress(
+			"",
+			"Calle Benito Juarez",
+			"El Centro",
+		)
+
+		require.EqualError(t, err, "address should have: street number")
+	})
+
+	t.Run("try parse address with empty street name", func(t *testing.T) {
+		_, err := vo.TryParseAddress(
+			"1",
+			"",
+			"El Centro",
+		)
+
+		require.EqualError(t, err, "address should have: street name")
+	})
+
+	t.Run("try parse address with empty street name", func(t *testing.T) {
+		_, err := vo.TryParseAddress(
+			"1",
+			"Calle Benito Juarez",
+			"",
+		)
+
+		require.EqualError(t, err, "address should have: suburb name")
+	})
+
+	t.Run("try parse address with empty street name", func(t *testing.T) {
+		_, err := vo.TryParseAddress(
+			"",
+			"",
+			"",
+		)
+
+		require.EqualError(t, err, "address should have: street number, street name, suburb name")
+	})
+}
